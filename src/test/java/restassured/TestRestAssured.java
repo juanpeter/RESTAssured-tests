bd9e3b4fab1aba386adf8170270bf4a7d149822b
João Pedro Israel <juanpeterisrael@gmail.com>
2020-08-22 16:32:28 -0300

    Done with POST and DELETE

--- src/test/java/restassured/TestRestAssured.java
+++ src/test/java/restassured/TestRestAssured.java
@@ -2,10 +2,13 @@ package restassured;
 
 import static io.restassured.RestAssured.given;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import restassured.UserResponse;
 
 public class TestRestAssured {
 	
@@ -29,5 +32,31 @@ public class TestRestAssured {
 			.get("https://reqres.in/api/users/23")
 		.then().contentType("application/json")
 			.statusCode(404).and().log().all();
+	}
+	
+	@Test
+	public void methodPost() {
+		User user = new User("John", "Doist");
+			
+			UserResponse response = RestAssured
+			.given().contentType("application/json")
+			.body(user)
+			.when().post("api/users/")
+			.then().contentType("application/json")
+			.statusCode(201).extract().as(UserResponse.class);
+		Assert.assertNotNull(response.getId());
+		Assert.assertNotNull(response.getCreatedAt());
+		Assert.assertEquals(user.getName(), response.getName());
+		Assert.assertEquals(user.getJob(), response.getJob());
+
+	}
+	
+	@Test
+	public void methodDelete() {
+		RestAssured.given()
+		.contentType(ContentType.JSON)
+		.when()
+			.delete("https://reqres.in/api/users/2")
+		.then().statusCode(204);
 	}
 }
